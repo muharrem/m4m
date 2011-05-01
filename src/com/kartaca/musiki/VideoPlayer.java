3035f1282a668b1aadecfeb5c62c57b18cbb6a02
Muharrem <muharremdemir@live.com>
2011-05-01 15:29:26 +0300

    project is initialized

---
+++ src/com/kartaca/musiki/VideoPlayer.java
@@ -0,0 +1,13 @@
+package com.kartaca.musiki;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideoPlayer extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
